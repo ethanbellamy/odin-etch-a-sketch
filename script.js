ec13f4e988e05590c5c04277221e40a8754e74a4
Ethan Bellamy <ethanbellamy@gmail.com>
2022-08-12 22:16:03 +0100

    Arrange grid squares using flexbox instead as inline-block causes gaps between rows

--- script.js
+++ script.js
@@ -2,11 +2,12 @@ container = document.querySelector('#grid-container');
 
 for (let i = 1; i <= 16; i++) {
     rowContainer = document.createElement('div');
+    rowContainer.style.display = 'flex';
     container.appendChild(rowContainer);
 
     for (let j = 1; j <= 16; j++) {
         div = document.createElement('div');
-        div.style.cssText = 'width: 32px; height: 32px; display: inline-block';
+        div.style.cssText = 'width: 32px; height: 32px';
         div.classList.add('square');
         rowContainer.appendChild(div);
     }
